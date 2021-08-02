af8c92baa2e5a4016367bcfc4239b228ea7c40b4
Metehan Saçakçı <63502128+msacakci@users.noreply.github.com>
2021-08-02 17:34:57 +0300

    MS020820211734

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,14 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SnowBlaze1.API", Version = "v1"});
+            });
+
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
