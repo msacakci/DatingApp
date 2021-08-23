41a069998873d3c6cc06c69f54268a71edfb59da
Metehan Saçakçı <63502128+msacakci@users.noreply.github.com>
2021-08-23 13:21:49 +0300

    Applied fix

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -16,6 +16,7 @@ export class MemberMessagesComponent implements OnInit
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) 
   {
@@ -28,9 +29,10 @@ export class MemberMessagesComponent implements OnInit
 
   sendMessage()
   {
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm.reset();
-    })
+    }).finally(() => this.loading = false);
   }
   
 
